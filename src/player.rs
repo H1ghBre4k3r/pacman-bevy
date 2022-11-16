086036587ea6c457f1da4a6eab778dd9048c1aa3
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-11-16 19:48:46 +0100

    Add direction and handle input accordingly

--- src/player.rs
+++ src/player.rs
@@ -6,7 +6,8 @@ pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(spawn_player);
+        app.add_startup_system(spawn_player)
+            .add_system(check_for_input);
     }
 }
 
@@ -18,14 +19,88 @@ fn spawn_player(mut commands: Commands, ascii: Res<AsciiSheet>) {
     sprite.custom_size = Some(Vec2::splat(1.));
     sprite.anchor = Anchor::BottomLeft;
 
-    commands.spawn(Pacman).insert(SpriteSheetBundle {
-        transform: Transform {
-            translation: Vec3::new(1.0, 1.0, 10.0),
-            scale: Vec3::new(1.0, 1.0, 0.0),
+    commands
+        .spawn(Pacman)
+        .insert(SpriteSheetBundle {
+            transform: Transform {
+                translation: Vec3::new(1.0, 1.0, 10.0),
+                scale: Vec3::new(1.0, 1.0, 0.0),
+                ..default()
+            },
+            sprite,
+            texture_atlas: ascii.0.clone(),
             ..default()
-        },
-        sprite,
-        texture_atlas: ascii.0.clone(),
-        ..default()
-    });
+        })
+        .insert(DirectionWrapper::default());
+}
+
+#[derive(Clone, Copy, Debug)]
+enum PlayerDirection {
+    Up,
+    Right,
+    Left,
+    Down,
+}
+
+#[derive(Component, Debug, Default)]
+struct DirectionWrapper {
+    pub direction: Option<PlayerDirection>,
+}
+
+impl DirectionWrapper {
+    pub fn set(&mut self, direction: Option<PlayerDirection>) {
+        debug!("DirectionWrapper::set({:?})", direction);
+        self.direction = direction;
+    }
+}
+
+impl Into<Vec3> for PlayerDirection {
+    fn into(self) -> Vec3 {
+        match self {
+            Self::Up => Vec3 {
+                x: 0.0,
+                y: 1.0,
+                z: 0.0,
+            },
+            Self::Right => Vec3 {
+                x: 1.0,
+                y: 0.0,
+                z: 0.0,
+            },
+            Self::Down => Vec3 {
+                x: 0.0,
+                y: -1.0,
+                z: 0.0,
+            },
+            Self::Left => Vec3 {
+                x: -1.0,
+                y: 0.0,
+                z: 0.0,
+            },
+        }
+    }
+}
+
+fn check_for_input(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut direction_query: Query<&mut DirectionWrapper, With<Pacman>>,
+) {
+    let mut direction = direction_query.get_single_mut().unwrap();
+
+    // TODO: should this be exclusive?
+    if keyboard_input.just_pressed(KeyCode::W) {
+        direction.set(Some(PlayerDirection::Up));
+    }
+    if keyboard_input.just_pressed(KeyCode::A) {
+        direction.set(Some(PlayerDirection::Left));
+    }
+    if keyboard_input.just_pressed(KeyCode::S) {
+        direction.set(Some(PlayerDirection::Down));
+    }
+    if keyboard_input.just_pressed(KeyCode::D) {
+        direction.set(Some(PlayerDirection::Right));
+    }
+    if keyboard_input.just_pressed(KeyCode::Space) {
+        direction.set(None);
+    }
 }
