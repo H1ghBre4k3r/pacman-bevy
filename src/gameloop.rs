32c53c8f7925d05155f34e960aa2ed3d00cb6a06
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-11-16 22:10:54 +0100

    Check for wall collisions and for out of bounds

--- src/gameloop.rs
+++ src/gameloop.rs
@@ -1,6 +1,10 @@
 use bevy::{prelude::*, time::FixedTimestep};
 
-use crate::player::{DirectionWrapper, Pacman};
+use crate::{
+    map::{WallPart, WallTile},
+    player::{DirectionWrapper, Pacman},
+    view::{COLUMNS, ROWS},
+};
 
 const TICK_TIME: f64 = 1.0 / 2.0;
 
@@ -20,11 +24,40 @@ impl Plugin for GameLoop {
 /// Move the player according to its current position
 fn move_player(
     mut transform_direction_query: Query<(&mut Transform, &DirectionWrapper), With<Pacman>>,
+    wall_query: Query<&Transform, (With<WallTile>, Without<Pacman>)>,
 ) {
     let (mut transform, direction_wrapper) = transform_direction_query.get_single_mut().unwrap();
     let direction = direction_wrapper.direction;
     let Some(direction) = direction else {
         return;
     };
-    transform.translation += direction;
+    // calculate the new position according to the current direction
+    let mut new_position = transform.translation + direction;
+
+    // we convert x and y to integers so we avoid floating point errors
+    let x = new_position.x as i32;
+    let y = new_position.y as i32;
+
+    // check for collisions with each wall
+    for wall in &wall_query {
+        let wall_position = wall.translation;
+        if wall_position.x.floor() as i32 == x && wall_position.y.floor() as i32 == y {
+            return;
+        }
+    }
+
+    // check for "going out of bounds"
+    if x < 0 {
+        new_position.x = (COLUMNS - 1) as f32;
+    } else if x >= COLUMNS as i32 {
+        new_position.x = 0.0;
+    }
+
+    if y < 0 {
+        new_position.y = (ROWS - 1) as f32;
+    } else if y >= ROWS as i32 {
+        new_position.y = 0.0;
+    }
+
+    transform.translation = new_position;
 }
